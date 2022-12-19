a9e08e2d216a75dbd527da9b841ee9bbd14264a1
Charptr0 <CharlesOfficial1@hotmail.com>
2022-12-18 22:34:00 -0500

    added sign in with google

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,7 @@
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import './App.css';
+import { GoogleOAuthProvider } from '@react-oauth/google';
+import config from "./config.json";
 import Dashboard from './Pages/Dashboard/Dashboard';
 import Home from './Pages/Home/Home';
 import Login from './Pages/Login/Login';
@@ -7,14 +9,16 @@ import Register from './Pages/Register/Register';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-      </Routes>
-    </BrowserRouter>
+    <GoogleOAuthProvider clientId={config.GOOGLE_AUTH_CLIENT_ID}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Routes>
+      </BrowserRouter>
+    </GoogleOAuthProvider>
   );
 }
 
--- frontend/src/Pages/Login/Login.tsx
+++ frontend/src/Pages/Login/Login.tsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { useRef, useState } from "react"
 import { useNavigate } from "react-router-dom";
+import { GoogleLogin } from '@react-oauth/google';
 import styles from "./Login.module.scss";
 
 export default function Login() {
@@ -19,7 +20,10 @@ export default function Login() {
         const email = emailRef.current?.value;
         const password = passwordRef.current?.value;
 
-        if (!email || !password) return;
+        if (!email || !password) {
+            setSubmitButtonDisabled(false);
+            return;
+        };
 
         const req = {
             email,
@@ -28,6 +32,7 @@ export default function Login() {
 
         try {
             const res = await axios.post("http://localhost:4001/login", req);
+            localStorage.setItem("token", res.data);
             navigate("/dashboard");
 
         } catch (err: any) {
@@ -41,10 +46,27 @@ export default function Login() {
             else {
                 console.log(`Internal Server Error`);
             }
-
         }
 
         setSubmitButtonDisabled(false);
+    }
+
+    async function googleLoginOnSuccess(credentialResponse: any) {
+
+        try {
+            const res = await axios.post("http://localhost:4001/loginOauth", credentialResponse);
+            console.log(credentialResponse);
+
+            localStorage.setItem("token", res.data);
+            navigate("/dashboard");
+
+        } catch (err: any) {
+
+        }
+    }
+
+    function googleLoginOnError() {
+
     }
 
     return <div id={styles.loginScreen}>
@@ -62,7 +84,8 @@ export default function Login() {
                 <button disabled={submitButtonDisabled}>Login</button>
                 <button disabled={submitButtonDisabled} type="button">Register</button>
             </div>
-
         </form>
+
+        <GoogleLogin onSuccess={googleLoginOnSuccess} onError={() => googleLoginOnError} />
     </div>
 }
