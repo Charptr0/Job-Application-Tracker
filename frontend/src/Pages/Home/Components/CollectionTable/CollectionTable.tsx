a23134087e31a38309f48d351875b79f442c7037
Charptr0 <CharlesOfficial1@hotmail.com>
2023-01-06 20:51:55 -0500

    adjust for mobile

--- frontend/src/Pages/Home/Components/CollectionTable/CollectionTable.tsx
+++ frontend/src/Pages/Home/Components/CollectionTable/CollectionTable.tsx
@@ -2,6 +2,7 @@ import { useRef, useState } from "react";
 import { randomCollection } from "../../Utils/randomCollection";
 import { randomJobApplication } from "../../Utils/randomJobApplication";
 import styles from "./CollectionTable.module.scss";
+import tableStyles from "../tableStyles.module.scss";
 
 interface ISampleApplication {
     companyName: string;
@@ -72,30 +73,30 @@ export default function CollectionTable() {
                 })}
             </select>
 
-            <table className={styles.table}>
+            <table className={tableStyles.table}>
                 <tbody>
                     <tr>
-                        <th>Company Name</th>
-                        <th>Job Title</th>
-                        <th>Job Type</th>
-                        <th>Location</th>
-                        <th>Application Link</th>
-                        <th>Status</th>
+                        <th className={tableStyles.col1}>Company Name</th>
+                        <th className={tableStyles.col2}>Job Title</th>
+                        <th className={tableStyles.col3}>Job Type</th>
+                        <th className={tableStyles.col4}>Location</th>
+                        <th className={tableStyles.col5}>Application Link</th>
+                        <th className={tableStyles.col6}>Status</th>
                     </tr>
                 </tbody>
                 {displayApplication.map((app, i) => {
                     return <tr key={i}>
-                        <td>{app.companyName}</td>
-                        <td>{app.jobTitle}</td>
-                        <td>{app.jobType}</td>
-                        <td>{app.location}</td>
-                        <td><a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" target="_blank" rel="noreferrer">Link</a></td>
-                        <td>{app.status}</td>
+                        <td className={tableStyles.col1}>{app.companyName}</td>
+                        <td className={tableStyles.col2}>{app.jobTitle}</td>
+                        <td className={tableStyles.col3}>{app.jobType}</td>
+                        <td className={tableStyles.col4}>{app.location}</td>
+                        <td className={tableStyles.col5}><a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" target="_blank" rel="noreferrer">Link</a></td>
+                        <td className={tableStyles.col6}>{app.status}</td>
                     </tr>
                 })}
             </table>
 
-            <div className={styles.btnContainer}>
+            <div className={tableStyles.btnContainer}>
                 <button onClick={createCollectionHandler}>Create</button>
                 <button onClick={() => setSampleCollections([])}>Reset</button>
             </div>
--- frontend/src/Pages/Home/Components/StaticTable/StaticTable.tsx
+++ frontend/src/Pages/Home/Components/StaticTable/StaticTable.tsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import { randomJobApplication } from "../../Utils/randomJobApplication";
 import styles from "./StaticTable.module.scss";
+import tableStyles from "../tableStyles.module.scss";
+
 
 interface ISampleApplication {
     companyName: string;
@@ -19,31 +21,31 @@ export default function StaticTable() {
 
     return (
         <>
-            <table className={styles.table}>
+            <table className={tableStyles.table}>
                 <tbody>
                     <tr>
-                        <th>Company Name</th>
-                        <th>Job Title</th>
-                        <th>Job Type</th>
-                        <th>Location</th>
-                        <th>Application Link</th>
-                        <th>Status</th>
+                        <th className={tableStyles.col1}>Company Name</th>
+                        <th className={tableStyles.col2}>Job Title</th>
+                        <th className={tableStyles.col3}>Job Type</th>
+                        <th className={tableStyles.col4}>Location</th>
+                        <th className={tableStyles.col5}>Application Link</th>
+                        <th className={tableStyles.col6}>Status</th>
                     </tr>
 
                     {sampleApplications.map((app: ISampleApplication, i) => {
                         return <tr key={i}>
-                            <td>{app.companyName}</td>
-                            <td>{app.jobTitle}</td>
-                            <td>{app.jobType}</td>
-                            <td>{app.location}</td>
-                            <td><a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" target="_blank" rel="noreferrer">Link</a></td>
-                            <td>{app.status}</td>
+                            <td className={tableStyles.col1}>{app.companyName}</td>
+                            <td className={tableStyles.col2}>{app.jobTitle}</td>
+                            <td className={tableStyles.col3}>{app.jobType}</td>
+                            <td className={tableStyles.col4}>{app.location}</td>
+                            <td className={tableStyles.col5}><a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" target="_blank" rel="noreferrer">Link</a></td>
+                            <td className={tableStyles.col6}>{app.status}</td>
                         </tr>
                     })}
                 </tbody>
             </table>
 
-            <div className={styles.btnContainer}>
+            <div className={tableStyles.btnContainer}>
                 <button onClick={createApplicationHandler}>Create</button>
                 <button onClick={() => setSampleApplications([])}>Reset</button>
             </div>
