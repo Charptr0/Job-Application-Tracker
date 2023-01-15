1d08977af2b9d7698fd6a517de9024e8c46782f6
Charptr0 <CharlesOfficial1@hotmail.com>
2023-01-14 20:24:38 -0500

    added warning messages for creating a new collection

--- frontend/src/Pages/Dashboard/Components/CreateCollection/CreateCollection.tsx
+++ frontend/src/Pages/Dashboard/Components/CreateCollection/CreateCollection.tsx
@@ -1,6 +1,7 @@
-import { useContext, useRef } from "react";
+import { useContext, useRef, useState } from "react";
 import { UserContext } from "../../../../Context/UserContext";
 import { addCollectionRequest } from "../../../../Utils/Requests/addCollection";
+import { fetchAllCollectionsRequest } from "../../../../Utils/Requests/fetchAllCollections";
 import { setCollection } from "../../../../Utils/Storage/setCollection";
 import modalStyles from "../../Utils/Styles/modal.module.scss";
 import styles from "./CreateCollection.module.scss";
@@ -13,20 +14,48 @@ export default function CreateCollection(props: IProps) {
     const collectionNameRef = useRef<HTMLInputElement>(null);
     const { currentUser, updateUser } = useContext<any>(UserContext);
 
-    async function submitHandler() {
+    const [disableSubmitButton, setDisableSubmitButton] = useState(false);
+    const [warningMessage, setWarningMessage] = useState("");
+
+
+    async function submitHandler(e: React.FormEvent<HTMLFormElement>) {
+        e.preventDefault();
+
         const collectionName = collectionNameRef.current?.value;
 
-        if (!collectionName || collectionName.toLowerCase() === 'all') {
+        if (!collectionName) {
+            setWarningMessage("This field cannot be empty");
             return;
         }
 
+        if (collectionName.toLowerCase() === 'all') {
+            setWarningMessage(`The new collection name cannot be named ${collectionName}`);
+            return;
+        }
+
+        setDisableSubmitButton(true);
+
         try {
+            const allCollections: string[] = await fetchAllCollectionsRequest(currentUser.id);
+
+            // make sure that the new collection does not exist already
+            for (const userCollectionName of allCollections) {
+
+                if (userCollectionName === collectionName) {
+                    setWarningMessage("This collection already exists");
+                    setDisableSubmitButton(false);
+                    return;
+                }
+            }
+
+            // make request to create new collection
             await addCollectionRequest(currentUser.id, collectionName);
             setCollection(collectionName);
         } catch (err) {
             console.error(err);
         }
 
+        setDisableSubmitButton(false);
         props.setVisible(false);
     }
 
@@ -34,12 +63,13 @@ export default function CreateCollection(props: IProps) {
         <div className={modalStyles.smallModal}>
             <h1>Create a New Collection</h1>
             <form onSubmit={submitHandler} className={styles.form}>
-                <label>Enter a New Collection Name</label><br></br>
+                <label>New Collection Name</label><br></br>
                 <input ref={collectionNameRef} /> <br></br>
+                {warningMessage.length > 0 && <div className={styles.warningMessage}>{warningMessage}</div>}
 
                 <div className={styles.btnContainer}><br></br>
                     <button type="button" onClick={() => props.setVisible(false)}>Cancel</button>
-                    <button>Create</button>
+                    <button disabled={disableSubmitButton}>Create</button>
                 </div>
             </form>
 
